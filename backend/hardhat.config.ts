62c2b4ec19b67cc21179bfa7922adea119f71c8f
Nicolas Coquelet <nicolascoquelet@gmail.com>
2023-11-28 13:33:45 +0100

    hadhat config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,9 +1,33 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-ignition';
+import '@nomicfoundation/hardhat-verify';
 import 'solidity-docgen';
 
+const { MNEMONIC, ALCHEMY_RPC_URL, ALCHEMY_ID, ETHERSCAN_ID } = process.env;
+const SEPOLIA_RPC_URL = `${ALCHEMY_RPC_URL}/${ALCHEMY_ID}`;
+
 const config: HardhatUserConfig = {
-  solidity: '0.8.20',
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      chainId: 11155111,
+    },
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_ID,
+  },
+
+  solidity: {
+    version: '0.8.20',
+    settings: {
+      optimizer: {
+        enabled: false,
+        runs: 200,
+      },
+    },
+  },
 };
 
 export default config;

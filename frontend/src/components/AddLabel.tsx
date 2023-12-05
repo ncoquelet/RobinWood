7575fb24699ae545d835649253a6b6d6f5382dce
Nicolas Coquelet <nicolascoquelet@gmail.com>
2023-12-05 01:48:26 +0100

    Label - Soumettre un nouveau label #36

--- frontend/src/components/AddLabel.tsx
+++ frontend/src/components/AddLabel.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {
+  Box,
   Button,
   Drawer,
   DrawerBody,
@@ -29,23 +30,35 @@ const AddLabel: FC = () => {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = useRef<HTMLButtonElement>(null);
+  const firstField = React.useRef<HTMLInputElement>(null);
+  const [loading, setloading] = useState(false);
 
   const {
     register,
     handleSubmit,
+    reset,
     setError,
     control,
     formState: { errors, isSubmitting },
   } = useForm<LabelFormData>();
 
   const handleFormSubmit = handleSubmit(async (data) => {
+    setloading(true);
     try {
       await addNewLabel(data);
+      reset();
       onClose();
     } catch (e) {
       console.log("error " + e);
+    } finally {
+      setloading(false);
     }
   });
+
+  const beforeOnClose = () => {
+    reset();
+    onClose();
+  };
 
   return (
     <>
@@ -55,82 +68,92 @@ const AddLabel: FC = () => {
       <Drawer
         isOpen={isOpen}
         placement="right"
-        onClose={onClose}
+        onClose={beforeOnClose}
         closeOnOverlayClick={false}
+        initialFocusRef={firstField}
         finalFocusRef={btnRef}
+        size="xl"
       >
         <DrawerOverlay />
         <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Submit a new label</DrawerHeader>
+          <Box style={loading ? { pointerEvents: "none", opacity: 0.4 } : {}}>
+            <DrawerCloseButton />
+            <DrawerHeader>Submit a new label</DrawerHeader>
 
-          <DrawerBody>
-            <form id="new-label-form" onSubmit={handleFormSubmit}>
-              <FormControl isRequired>
-                <FormLabel>Label name</FormLabel>
-                <Input
-                  type="text"
-                  placeholder="Enter you label name"
-                  {...register("name", {
-                    required: true,
-                  })}
-                />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel>Description</FormLabel>
-                <Textarea
-                  placeholder="Describe your label"
-                  size="sm"
-                  {...register("description", {
-                    required: true,
-                  })}
-                />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel>Geographical area</FormLabel>
-                <Select
-                  placeholder="Select a zone"
-                  {...register("geoArea", {
-                    required: true,
-                  })}
+            <DrawerBody>
+              <form id="new-label-form" onSubmit={handleFormSubmit}>
+                <FormControl isRequired>
+                  <FormLabel>Label name</FormLabel>
+                  <Input
+                    type="text"
+                    placeholder="Enter you label name"
+                    {...register("name", {
+                      required: true,
+                    })}
+                  />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Description</FormLabel>
+                  <Textarea
+                    placeholder="Describe your label"
+                    size="sm"
+                    {...register("description", {
+                      required: true,
+                    })}
+                  />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Geographical area</FormLabel>
+                  <Select
+                    placeholder="Select a zone"
+                    {...register("geoArea", {
+                      required: true,
+                    })}
+                  >
+                    <option value="europe">Europe</option>
+                    <option value="world">World</option>
+                    <option value="north_america">North America</option>
+                    <option value="south_america">South America</option>
+                    <option value="asia">Asia</option>
+                    <option value="africa">Africa</option>
+                  </Select>
+                </FormControl>
+                <FileUpload
+                  name="logo"
+                  acceptedFileTypes="image/"
+                  placeholder="Select label logo"
+                  isRequired={true}
+                  control={control}
                 >
-                  <option value="europe">Europe</option>
-                  <option value="world">World</option>
-                  <option value="north_america">North America</option>
-                  <option value="south_america">South America</option>
-                  <option value="asia">Asia</option>
-                  <option value="africa">Africa</option>
-                </Select>
-              </FormControl>
-              <FileUpload
-                name="logo"
-                acceptedFileTypes="image/"
-                placeholder="Select label logo"
-                isRequired={true}
-                control={control}
-              >
-                Logo
-              </FileUpload>
-              <FileUpload
-                name="document"
-                acceptedFileTypes="application/msword, application/pdf"
-                placeholder="Select label document ..."
-                isRequired={true}
-                control={control}
-              >
-                Label Document
-              </FileUpload>
-            </form>
-          </DrawerBody>
+                  Logo
+                </FileUpload>
+                <FileUpload
+                  name="document"
+                  acceptedFileTypes="application/msword, application/pdf"
+                  placeholder="Select label document ..."
+                  isRequired={true}
+                  control={control}
+                >
+                  Label Document
+                </FileUpload>
+              </form>
+            </DrawerBody>
 
-          <DrawerFooter>
-            <Button variant="outline" mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button type="submit" form="new-label-form" colorScheme="blue">
-              Save
-            </Button>
-          </DrawerFooter>
+            <DrawerFooter>
+              <Button variant="outline" mr={3} onClick={beforeOnClose}>
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                isLoading={loading}
+                loadingText="Submitting"
+                form="new-label-form"
+                colorScheme="blue"
+              >
+                Submit
+              </Button>
+            </DrawerFooter>
+          </Box>
         </DrawerContent>
       </Drawer>
     </>

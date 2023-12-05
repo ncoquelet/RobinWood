d041f1ab7059b3cf48ecc560b77d76b4bdb1a732
Nicolas Coquelet <nicolascoquelet@gmail.com>
2023-12-05 10:24:30 +0100

    Ne pas afficher de contenu si on n'est pas connecté

--- frontend/src/app/label/page.tsx
+++ frontend/src/app/label/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
 import AddLabel from "@/components/AddLabel";
+import DisconnectedCard from "@/components/DisconnectedCard";
 import { LabelProvider } from "@/context/labelContext";
+import { Card, CardBody, Text } from "@chakra-ui/react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
 
 export default function Label() {
+  const { address, isDisconnected } = useAccount(); // TODO: Handle isDisconnected and redirects
+
   return (
     <div>
       <LabelProvider>
-        <p>Labels</p>
-        <AddLabel />
+        {isDisconnected ? (
+          <DisconnectedCard />
+        ) : (
+          <>
+            <p>Labels</p>
+            <AddLabel />
+          </>
+        )}
       </LabelProvider>
     </div>
   );
---
+++ frontend/src/components/DisconnectedCard.tsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Center,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from "react";
+
+export default function DisconnectedCard() {
+  return (
+    <Center mt={200}>
+      <Card
+        maxW="xl"
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+      >
+        <Image
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src="/robinwood.png"
+          alt="Caffe Latte"
+        />
+
+        <Stack>
+          <CardBody>
+            <Heading size="md">Connection Required</Heading>
+
+            <Text py="2">
+              Your wallet is not connected. Please click the button below to
+              establish a connection.
+            </Text>
+          </CardBody>
+
+          <CardFooter>
+            <ConnectButton />
+          </CardFooter>
+        </Stack>
+      </Card>
+    </Center>
+  );
+}

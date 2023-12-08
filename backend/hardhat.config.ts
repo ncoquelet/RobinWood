3a8deba803358aa5886303d49e6702f3a01a843a
Nicolas Coquelet <nicolascoquelet@gmail.com>
2023-12-08 16:15:05 +0100

    try to remove sepolia config if not production

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -4,20 +4,24 @@ import '@nomicfoundation/hardhat-verify'
 import { HardhatUserConfig } from 'hardhat/config'
 import 'solidity-docgen'
 
-const { ACCOUNT0, ALCHEMY_SEPOLIA_RPC_URL, ETHERSCAN_ID } = process.env
+const { ACCOUNT0, ALCHEMY_SEPOLIA_RPC_URL, ALCHEMY_KEY, ETHERSCAN_API_KEY } =
+  process.env
+
+const networks: any = {}
+if (process.env.NODE_ENV === 'production') {
+  networks['sepolia'] = {
+    url: (ALCHEMY_SEPOLIA_RPC_URL as string) + ALCHEMY_KEY,
+    chainId: 11155111,
+    accounts: [ACCOUNT0 as string],
+  }
+}
 
 const config: HardhatUserConfig = {
   paths: {
     tests: './contracts',
   },
 
-  networks: {
-    sepolia: {
-      url: ALCHEMY_SEPOLIA_RPC_URL,
-      chainId: 11155111,
-      accounts: [ACCOUNT0 as string],
-    },
-  },
+  networks: networks,
 
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,

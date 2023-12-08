3fba9c0b821617f31fca65b33a786aef5454ea32
Nicolas Coquelet <nicolascoquelet@gmail.com>
2023-12-08 17:21:17 +0100

    HARDHAT_ENV

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -8,7 +8,7 @@ const { ACCOUNT0, ALCHEMY_SEPOLIA_RPC_URL, ALCHEMY_KEY, ETHERSCAN_API_KEY } =
   process.env
 
 const networks: any = {}
-if (process.env.NODE_ENV === 'production') {
+if (process.env.HARDHAT_ENV === 'production') {
   networks['sepolia'] = {
     url: (ALCHEMY_SEPOLIA_RPC_URL as string) + ALCHEMY_KEY,
     chainId: 11155111,
